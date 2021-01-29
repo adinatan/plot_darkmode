885f58f64972d5385c4d48d7c4a08093fe299dfd
Adi Natan <natan@stanford.edu>
2021-01-28 16:13:56 -0800

    Add files via upload

--- plot_darkmode.m
+++ plot_darkmode.m
@@ -1,19 +1,24 @@
 function  plot_darkmode(varargin)
 % The function generates from a Matlab plot figure a version that can be
 % copied to a dark mode theme presentation or website.
-% The function replaces the default texts and boxes color to
-% a user input color (default is white), make the plot area transparent
-% and brighten plot line colors if there are too dim (set by the user)
+% The function replaces the default texts and box colors to
+% a user input color (default is white), make the plot area transparent to
+% accept the dark background below it. The function also transform that
+% graphic colors that are not appropriate (low contrast) to a version that
+% dark theme legible using a desaturation and brightness approach.
 %
-% The function doesn't support tiledlayout at the moment.
-
+% preparing this function I was inspired by https://material.io/design/color/dark-theme.html
+%
+% The function doesn't support all figure objects (such as tiledlayout)
 
 %  Inputs:
 %  varargin(1)- The text color to modify (default is white)
-%  varargin(2)- The threshold from which to apply the brightness correction (default is <0.5)
-%  varargin(3)- The Gamma factor that modifies the brightness (default is 0.5)
+%  varargin(2)- The threshold from which to apply the cotrast correction (default is 4.5)
+%  varargin(3)- The dark background  (default is gray of value 0.16)
 %
-%   Example:
+%
+%  How to the function:
+%  generate or import a Matlab figure and run the function:
 %
 %       plot(bsxfun(@times,[1:4],[2:5]'));xlabel('X');ylabel('Y');
 %       plot_darkmode
@@ -31,23 +36,24 @@ function  plot_darkmode(varargin)
 switch nargin
     case 3
         textcolor=varargin(1);
-        brightness_threshold=varargin(2);
-        GammaFactor=varargin(3);
+        contrast_ratio=varargin(2);
+        dark_bkg_assumption=varargin(3);
     case 2
         textcolor=varargin(1);
-        brightness_threshold=varargin(2);
-        GammaFactor=0.5;
+        contrast_ratio=varargin(2);
+        dark_bkg_assumption= ones(1,3)*0.16;
     case 1
         textcolor=varargin(1);
-        brightness_threshold=0.5;
-        GammaFactor=0.5;
+        contrast_ratio=4.5;
+        dark_bkg_assumption= ones(1,3)*0.16;
     otherwise
         textcolor=[1,1,1];
-        brightness_threshold=0.5;
-        GammaFactor=0.5;
+        contrast_ratio=4.5;
+        dark_bkg_assumption= ones(1,3)*0.16;
 end
 
-brightness = @(color) 0.2989*color(1) + 0.5870*color(2) +0.1140*color(3);
+tcd = [{textcolor} , {contrast_ratio} , {dark_bkg_assumption}];
+
 
 
 g = get(get(gcf,'children'),'type');
@@ -83,12 +89,20 @@ for n=1:numel(axes_ind)
     
     h(axes_ind(n)).Title.Color  = textcolor; % edit title text color
     
+    h(axes_ind(n)).GridColor =  adjust_color(h(axes_ind(n)).GridColor,tcd);
+    h(axes_ind(n)).MinorGridColor =  adjust_color(h(axes_ind(n)).MinorGridColor,tcd);
+    %h(axes_ind(n)).Subtitle.Color = textcolor;
+    
     % take care of other axes children:
     h2 = get(h(axes_ind(n)),'Children');
     g2 = get(h(axes_ind(n)).Children,'type');
     text_ind  = find(strcmp(g2,'text'));
     patch_ind = find(strcmp(g2,'patch'));
     line_ind  = find(strcmp(g2,'line'));
+    errorbar_ind = find(strcmp(g2,'errorbar'));
+    area_ind  = find(strcmp(g2,'area'));
+    bar_ind  = find(strcmp(g2,'bar'));
+    %contour_ind  = find(strcmp(g2,'contour'));
     
     % edit texts color
     for m=1:numel(text_ind)
@@ -98,39 +112,39 @@ for n=1:numel(axes_ind)
     % brighten patch colors if dim (use for the case of arrows etc)
     % this might not work well for all patch types so consider to comment
     for m=1:numel(patch_ind)
-        if brightness(h2(patch_ind(m)).FaceColor)<brightness_threshold &&...
-                brightness(h2(patch_ind(m)).FaceColor)>0
-            
-            
-            h2(patch_ind(m)).FaceColor=h2(patch_ind(m)).FaceColor.^GammaFactor;
-            
-        elseif brightness(h2(patch_ind(m)).FaceColor)<eps
-            
-            h2(patch_ind(m)).FaceColor = textcolor./2 ;
-        end
-        
-        h2(patch_ind(m)).EdgeColor=textcolor;
-        
+        h2(patch_ind(m)).FaceColor = adjust_color(h2(patch_ind(m)).FaceColor,tcd);
+        h2(patch_ind(m)).EdgeColor = adjust_color(h2(patch_ind(m)).EdgeColor,tcd);
     end
     
-    % brighten line plot colors if dim
     for m=1:numel(line_ind)
-        if brightness(h2(line_ind(m)).Color)<brightness_threshold &&...
-                brightness(h2(line_ind(m)).Color)>0
-            
-            h2(line_ind(m)).Color=h2(line_ind(m)).Color.^GammaFactor;
-            
-            % special case if the line is black
-        elseif brightness(h2(line_ind(m)).Color)<eps
-            
-            h2(line_ind(m)).Color= textcolor./2 ;
-            
-        end
+        h2(line_ind(m)).Color = adjust_color(h2(line_ind(m)).Color,tcd);
     end
+    
+    
+    for m=1:numel(errorbar_ind)
+        h2(errorbar_ind(m)).Color = adjust_color(h2(errorbar_ind(m)).Color,tcd);
+        h2(errorbar_ind(m)).MarkerEdgeColor =   adjust_color(h2(errorbar_ind(m)).MarkerEdgeColor,tcd);
+        h2(errorbar_ind(m)).MarkerFaceColor = adjust_color(h2(errorbar_ind(m)).MarkerFaceColor,tcd);
+    end
+    
+    for m=1:numel(area_ind)
+        h2(area_ind(m)).FaceColor = adjust_color(h2(area_ind(m)).FaceColor,tcd);
+        h2(area_ind(m)).EdgeColor = adjust_color(h2(area_ind(m)).EdgeColor,tcd);
+    end
+    
+    for m=1:numel(bar_ind)
+        h2(bar_ind(m)).FaceColor = adjust_color(h2(bar_ind(m)).FaceColor,tcd);
+        h2(bar_ind(m)).EdgeColor = adjust_color(h2(bar_ind(m)).EdgeColor,tcd);
+    end
+    
+%       for m=1:numel(contour_ind)
+%         h2(contour_ind(m)).FaceColor = adjust_color(h2(contour_ind(m)).FaceColor,tcd);
+%         h2(contour_ind(m)).EdgeColor = adjust_color(h2(contour_ind(m)).EdgeColor,tcd);
+%     end
+    
+    
 end
-
 %% modify Colorbars:
-
 for n=1:numel(colorbar_ind)
     h(colorbar_ind(n)).Color        =  textcolor;
     h(colorbar_ind(n)).Label.Color  =  textcolor;
@@ -144,6 +158,7 @@ for n=1:numel(legend_ind)
     h(legend_ind(n)).Box       = 'off';      % delete box
 end
 
+
 %% modify annotations:
 
 ha=findall(gcf,'Tag','scribeOverlay');
@@ -151,21 +166,163 @@ ha=findall(gcf,'Tag','scribeOverlay');
 if ~isempty(ha)
     for n=1:numel(ha)
         hAnnotChildren = get(ha(n),'Children');
-        ga=get(get(ha(n),'Children'),'type');
-        
-        % edit textbox text colors
-        textbox_ind=find(strcmp(ga,'textboxshape'));
-        
-        for m=1:numel(textbox_ind)
-            hAnnotChildren(textbox_ind(m)).Color      =  textcolor;
-            hAnnotChildren(textbox_ind(m)).EdgeColor  =  textcolor;
+        try
+            hAnnotChildrenType=get(hAnnotChildren,'type');
+        catch
+            disp('annotation not available')
+            return
         end
         
         
-        % if there are more types of annotations use the textbox section
-        % as a template
+        % edit lineType and shapeType colors
+        textboxshape_ind        = find(strcmp(hAnnotChildrenType,'textboxshape'));
+        ellipseshape_ind        = find(strcmp(hAnnotChildrenType,'ellipseshape'));
+        rectangleshape_ind      = find(strcmp(hAnnotChildrenType,'rectangleshape'));
+        textarrowshape_ind      = find(strcmp(hAnnotChildrenType,'textarrowshape'));
+        doubleendarrowshape_ind = find(strcmp(hAnnotChildrenType,'doubleendarrowshape'));
+        arrowshape_ind          = find(strcmp(hAnnotChildrenType,'arrowshape'));
+        arrow_ind               = find(strcmp(hAnnotChildrenType,'Arrow')); % older Matlab ver
+        lineshape_ind           = find(strcmp(hAnnotChildrenType,'lineshape'));
+        
+        
+        for m=1:numel(textboxshape_ind)
+            hAnnotChildren(textboxshape_ind(m)).Color      =  textcolor;
+            hAnnotChildren(textboxshape_ind(m)).EdgeColor  =  textcolor;
+        end
+        
+        for m=1:numel(ellipseshape_ind)
+            hAnnotChildren(ellipseshape_ind(m)).Color      =  adjust_color(hAnnotChildren(ellipseshape_ind(m)).Color,tcd);
+            hAnnotChildren(ellipseshape_ind(m)).FaceColor  =  adjust_color(hAnnotChildren(ellipseshape_ind(m)).FaceColor,tcd);
+        end
+        
+        for m=1:numel(rectangleshape_ind)
+            hAnnotChildren(rectangleshape_ind(m)).Color      =  adjust_color(hAnnotChildren(rectangleshape_ind(m)).Color,tcd);
+            hAnnotChildren(rectangleshape_ind(m)).FaceColor  =  adjust_color(hAnnotChildren(rectangleshape_ind(m)).FaceColor,tcd);
+        end
+        
+        for m=1:numel(textarrowshape_ind)
+            hAnnotChildren(textarrowshape_ind(m)).Color      =  adjust_color(hAnnotChildren(textarrowshape_ind(m)).Color,tcd);
+            hAnnotChildren(textarrowshape_ind(m)).TextColor  =  textcolor;
+            hAnnotChildren(textarrowshape_ind(m)).TextEdgeColor = adjust_color(hAnnotChildren(textarrowshape_ind(m)).TextEdgeColor,tcd);
+        end
+        
+        for m=1:numel(doubleendarrowshape_ind)
+            hAnnotChildren(doubleendarrowshape_ind(m)).Color = adjust_color(hAnnotChildren(doubleendarrowshape_ind(m)).Color,tcd);
+        end
+        
+        for m=1:numel(arrowshape_ind)
+            hAnnotChildren(arrowshape_ind(m)).Color = adjust_color(hAnnotChildren(arrowshape_ind(m)).Color,tcd);
+        end
+        
+        for m=1:numel(arrow_ind)
+            hAnnotChildren(arrow_ind(m)).Color = adjust_color(hAnnotChildren(arrow_ind(m)).Color,tcd);
+        end
+        
+        for m=1:numel(lineshape_ind)
+            hAnnotChildren(lineshape_ind(m)).Color = adjust_color(hAnnotChildren(lineshape_ind(m)).Color,tcd);
+        end
+        
         
     end
     
     
 end
+
+function  out=adjust_color(in,tcd)
+% This function modifies an input color to fit a dark theme background.
+% For that a color needs to have sufficient contrast (WCAG's AA standard of at least 4.5:1)
+% The contrast ratio is calculate via :  cr = (L1 + 0.05) / (L2 + 0.05),
+% where L1 is the relative luminance of the input color and L2 is the
+% relative luminance of the dark mode background.
+% For this case we will assume a dark mode theme background of...
+% If a color is not passing this ratio, it will be modified to meet it
+% via desaturation and brightness to be more legible.
+% the function uses fminbnd, if you dont have the toolbox to use it you can
+% replace it with fmintx (avaiable in Matlab's file exchange) 
+
+% if color is 'none' return as is
+if strcmp(in,'none')
+    out=in;
+    return
+end
+dark_bkg_assumption=tcd{3};
+
+% find the perceived lightness which is measured by some vision models
+% such as CIELAB to approximate the human vision non-linear response curve.
+% 1. linearize the RGB values (sRGB2Lin)
+% 2. find Luminance (Y)
+% 3. calc the perceived lightness (Lstar)
+% Lstar is in the range 0 to 1 where 0.5 is the perceptual "middle gray".
+% see https://en.wikipedia.org/wiki/SRGB ,
+
+sRGB2Lin=@(in) (in./12.92).*(in<= 0.04045) +  ( ((in+0.055)./1.055).^2.4 ).*(in> 0.04045);
+%Y = @(in) sum(sRGB2Lin(in).*[0.2126,  0.7152,  0.0722 ]);
+Y = @(in) sum(bsxfun(@times,sRGB2Lin( in ),[0.2126,  0.7152,  0.0722 ]),2 );
+Lstar = @(in)  0.01.*( (Y(in).*903.3).*(Y(in)<= 0.008856) + (Y(in).^(1/3).*116-16).*(Y(in)>0.008856));
+
+%dark_bkg_assumption = (ones(1,3)*0.16); % can be modfied
+Ybkg = sum(sRGB2Lin(dark_bkg_assumption).*[0.2126,  0.7152,  0.0722 ]);
+
+cr = @(in)   (Y(in)' + 0.05) ./ (Ybkg + 0.05); % contrast ratio
+
+% rgb following desaturation of factor x
+ds=@(in,x) hsv2rgb( bsxfun(@times,rgb2hsv(in),[ones(numel(x),1) x(:) ones(numel(x),1)] ));
+
+% rgb following brightness change of factor x
+br=@(in,x) hsv2rgb( bsxfun(@power,rgb2hsv(in),[ones(numel(x),1) ones(numel(x),1) x(:)] ));
+
+
+if cr(in)<tcd{2} % default is 4.5
+    
+    %check if color is just black and replace with perceptual "middle gray"
+    if ~sum(in)
+        fun0 = @(x) abs(Lstar( (ones(1,3)*x-dark_bkg_assumption ))-0.5);
+        L_factor=fminbnd(fun0,0.3,1);
+        
+        out = ones(1,3)*L_factor;
+        return
+        
+    end
+    
+    
+    % if saturation is what reduce contrast desaturate
+    in_hsv=rgb2hsv(in);
+    if in_hsv(2)>0.5
+        
+        fun1=@(x) abs(cr(ds(in,x))-tcd{2});
+        [ds_factor, val]=fminbnd(fun1,0,in_hsv(2));
+        
+        if val<1e-2
+            out = ds(in,ds_factor);
+            return
+        end
+    end
+    
+    % desaturation alone didn't solve it, try increase brightness
+    fun2 =  @(x) abs(cr(br(in,x))-tcd{2});
+    [br_factor, val]=fminbnd(fun2,0,1);
+    
+    if val<1e-2 && Lstar(br(in,br_factor))>0.5
+        out = br(in,br_factor);
+        return
+    end
+    
+    % if niether worked then brightening + desaturation:
+    fun3 = @(x) abs(cr(ds(br(in,br_factor),x))-tcd{2});
+    [brds_factor, val]=fminbnd(fun3,0,1);
+    
+    if val<1e-2 && ds(br(in,br_factor),brds_factor)
+        out = ds(br(in,br_factor),brds_factor);
+        return
+        
+    end
+    
+    % if all fails treat the color as black as above:
+    fun0 = @(x) abs(Lstar( (ones(1,3)*x-dark_bkg_assumption ))-0.5);
+    L_factor=fminbnd(fun0,0.3,1);
+    out = ones(1,3)*L_factor;
+    
+else
+    out = in ;
+end
+
