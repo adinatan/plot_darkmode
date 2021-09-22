d043dd2006dbf749885454e659a0e4bd5e6be729
Adi Natan <natan@stanford.edu>
2021-09-22 15:47:14 -0700

    added tiledlayout functionality

--- plot_darkmode.m
+++ plot_darkmode.m
@@ -10,8 +10,8 @@ function  plot_darkmode(varargin)
 %
 % preparing this function I was inspired by https://material.io/design/color/dark-theme.html
 %
-% The function is a work in progess and doesn't support all figure objects
-% (such as tiledlayout, etc)
+% The function is a work in progess and may not support all figure objects
+
 
 %  Inputs:
 %  varargin(1)- The text color to modify (default is white)
@@ -31,7 +31,7 @@ function  plot_darkmode(varargin)
 %  Background is enabled
 
 
-%   Ver 1.01 (2021-02-25)
+%   Ver 1.02 (2021-09-22)
 %   Adi Natan (natan@stanford.edu)
 
 %% defaults
@@ -59,45 +59,50 @@ tcd = [{textcolor} , {contrast_ratio} , {dark_bkg_assumption}];
 
 
 g = get(get(gcf,'children'),'type');
-h = get(gcf,'children');
-
-axes_ind      =  find(strcmp(g,'axes'));
-legend_ind    =  find(strcmp(g,'legend'));
-colorbar_ind  =  find(strcmp(g,'colorbar'));
-%tiled_ind     =  find(strcmp(g,'tiledlayout'));
+if ~strcmp(g,'tiledlayout')
+    h = get(gcf,'children');
+    axes_ind      =  findobj(h,'type','Axes');
+    legend_ind    =  findobj(h,'type','Legend');
+    colorbar_ind  =  findobj(h,'type','Colorbar');
+else
+    h=get(get(gcf,'children'),'children');
+    axes_ind      =  findobj(h,'type','Axes');
+    legend_ind    =  findobj(h,'type','Legend');
+    colorbar_ind  =  findobj(h,'type','Colorbar');
+end
 
 
 %% modify Axes
 for n=1:numel(axes_ind)
     
     % edit x-ticks color
-    for m=1:numel(h(axes_ind(n)).XTickLabel)
-        h(axes_ind(n)).XTickLabel{m} = ['\color[rgb]' sprintf('{%f,%f,%f}%s',textcolor)   h(axes_ind(n)).XTickLabel{m} ];
+    for m=1:numel(axes_ind(n).XTickLabel)
+        axes_ind(n) .XTickLabel{m} = ['\color[rgb]' sprintf('{%f,%f,%f}%s',textcolor)    axes_ind(n) .XTickLabel{m} ];
     end
     
     % edit y-ticks color
-    for m=1:numel(h(axes_ind(n)).YTickLabel)
-        h(axes_ind(n)).YTickLabel{m} = ['\color[rgb]' sprintf('{%f,%f,%f}%s',textcolor)   h(axes_ind(n)).YTickLabel{m} ];
+    for m=1:numel( axes_ind(n).YTickLabel)
+        axes_ind(n).YTickLabel{m} = ['\color[rgb]' sprintf('{%f,%f,%f}%s',textcolor)    axes_ind(n).YTickLabel{m} ];
     end
     
-    h(axes_ind(n)).Color        = 'none';    % make white area transparent
-    h(axes_ind(n)).XColor       = textcolor; % edit x axis color
-    h(axes_ind(n)).YColor       = textcolor; % edit y axis color
-    h(axes_ind(n)).ZColor       = textcolor; % edit z axis color
+    axes_ind(n) .Color        = 'none';    % make white area transparent
+    axes_ind(n).XColor       = textcolor; % edit x axis color
+    axes_ind(n).YColor       = textcolor; % edit y axis color
+    axes_ind(n).ZColor       = textcolor; % edit z axis color
     
-    h(axes_ind(n)).XLabel.Color = textcolor; % edit x label color
-    h(axes_ind(n)).YLabel.Color = textcolor; % edit y label color
-    h(axes_ind(n)).ZLabel.Color = textcolor; % edit z label color
+    axes_ind(n).XLabel.Color = textcolor; % edit x label color
+    axes_ind(n).YLabel.Color = textcolor; % edit y label color
+    axes_ind(n).ZLabel.Color = textcolor; % edit z label color
     
-    h(axes_ind(n)).Title.Color  = textcolor; % edit title text color
+    axes_ind(n).Title.Color  = textcolor; % edit title text color
     
-    h(axes_ind(n)).GridColor =  adjust_color(h(axes_ind(n)).GridColor,tcd);
-    h(axes_ind(n)).MinorGridColor =  adjust_color(h(axes_ind(n)).MinorGridColor,tcd);
-    %h(axes_ind(n)).Subtitle.Color = textcolor;
+    axes_ind(n).GridColor =  adjust_color( axes_ind(n).GridColor,tcd);
+    axes_ind(n).MinorGridColor =  adjust_color(axes_ind(n).MinorGridColor,tcd);
+    % axes_ind(n).Subtitle.Color = textcolor;
     
     % take care of other axes children:
-    h2 = get(h(axes_ind(n)),'Children');
-    g2 = get(h(axes_ind(n)).Children,'type');
+    h2 = get(axes_ind(n),'Children');
+    g2 = get(axes_ind(n).Children,'type');
     text_ind  = find(strcmp(g2,'text'));
     patch_ind = find(strcmp(g2,'patch'));
     line_ind  = find(strcmp(g2,'line'));
@@ -111,8 +116,8 @@ for n=1:numel(axes_ind)
     for m=1:numel(text_ind)
         h2(text_ind(m)).Color=adjust_color( h2(text_ind(m)).Color ,tcd);
         if ~strcmp( h2(text_ind(m)).BackgroundColor,'none')
-            h2(text_ind(m)).BackgroundColor = tcd{3};    %if text has some background color switch to dark bkg theme 
-        end   
+            h2(text_ind(m)).BackgroundColor = tcd{3};    %if text has some background color switch to dark bkg theme
+        end
     end
     
     % brighten patch colors if dim (use for the case of arrows etc)
@@ -143,25 +148,25 @@ for n=1:numel(axes_ind)
         h2(bar_ind(m)).EdgeColor = adjust_color(h2(bar_ind(m)).EdgeColor,tcd);
     end
     
-%       for m=1:numel(contour_ind)
-%         h2(contour_ind(m)).FaceColor = adjust_color(h2(contour_ind(m)).FaceColor,tcd);
-%         h2(contour_ind(m)).EdgeColor = adjust_color(h2(contour_ind(m)).EdgeColor,tcd);
-%     end
+    %       for m=1:numel(contour_ind)
+    %         h2(contour_ind(m)).FaceColor = adjust_color(h2(contour_ind(m)).FaceColor,tcd);
+    %         h2(contour_ind(m)).EdgeColor = adjust_color(h2(contour_ind(m)).EdgeColor,tcd);
+    %     end
     
     
 end
 %% modify Colorbars:
 for n=1:numel(colorbar_ind)
-    h(colorbar_ind(n)).Color        =  textcolor;
-    h(colorbar_ind(n)).Label.Color  =  textcolor;
+    colorbar_ind(n).Color        =  textcolor;
+    colorbar_ind(n).Label.Color  =  textcolor;
 end
 
 %% modify Legends:
 
 for n=1:numel(legend_ind)
-    h(legend_ind(n)).Color     = 'none';     % make white area transparent
-    h(legend_ind(n)).TextColor = textcolor;  % edit text color
-    h(legend_ind(n)).Box       = 'off';      % delete box
+    legend_ind(n).Color     = 'none';     % make white area transparent
+    legend_ind(n).TextColor = textcolor;  % edit text color
+    legend_ind(n).Box       = 'off';      % delete box
 end
 
 
@@ -193,7 +198,7 @@ if ~isempty(ha)
         
         for m=1:numel(textboxshape_ind)
             hAnnotChildren(textboxshape_ind(m)).Color      =  textcolor;
-            hAnnotChildren(textboxshape_ind(m)).EdgeColor  =  textcolor;
+            hAnnotChildren(textboxshape_ind(m)).EdgeColor  =  adjust_color(hAnnotChildren(textboxshape_ind(m)).EdgeColor);
         end
         
         for m=1:numel(ellipseshape_ind)
@@ -244,7 +249,7 @@ function  out=adjust_color(in,tcd)
 % If a color is not passing this ratio, it will be modified to meet it
 % via desaturation and brightness to be more legible.
 % the function uses fminbnd, if you dont have the toolbox to use it you can
-% replace it with fmintx (avaiable in Matlab's file exchange) 
+% replace it with fmintx (avaiable in Matlab's file exchange)
 
 % if color is 'none' return as is
 if strcmp(in,'none')
@@ -252,8 +257,8 @@ if strcmp(in,'none')
     return
 end
 
-if isa(in,'char') ; % for 'flat' etc...
-   out=in;
+if isa(in,'char') % for inputs such as 'flat' etc...
+    out=in;
     return
 end
 
@@ -271,7 +276,7 @@ sRGB2Lin=@(in) (in./12.92).*(in<= 0.04045) +  ( ((in+0.055)./1.055).^2.4 ).*(in>
 %Y = @(in) sum(sRGB2Lin(in).*[0.2126,  0.7152,  0.0722 ]);
 Y = @(in) sum(bsxfun(@times,sRGB2Lin( in ),[0.2126,  0.7152,  0.0722 ]),2 );
 Lstar = @(in)  0.01.*( (Y(in).*903.3).*(Y(in)<= 0.008856) + (Y(in).^(1/3).*116-16).*(Y(in)>0.008856));
- 
+
 Ybkg = sum(sRGB2Lin(dark_bkg_assumption).*[0.2126,  0.7152,  0.0722 ]);
 
 cr = @(in)   (Y(in)' + 0.05) ./ (Ybkg + 0.05); % contrast ratio
