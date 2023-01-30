e673d9398ce1eb771595b614241b736a76fe6561
Adi Natan <natan@stanford.edu>
2023-01-29 21:08:54 -0800

    v1.04

--- plot_darkmode.m
+++ plot_darkmode.m
@@ -31,7 +31,7 @@ function  plot_darkmode(varargin)
 %  Background is enabled
 
 
-%   Ver 1.03 (2022-09-21)
+%   Ver 1.04 (2023-01-29)
 %   Adi Natan (natan@stanford.edu)
 
 %% defaults
@@ -65,14 +65,14 @@ if ~strcmp(g,'tiledlayout')
     legend_ind    =  findobj(h,'type','Legend');
     colorbar_ind  =  findobj(h,'type','Colorbar');
     polaraxes_ind=   findobj(h,'type','PolarAxes');
- 
+
 else
     h0= get(gcf,'children');
     h0.Title.Color    =  textcolor;
     h0.Subtitle.Color =  textcolor;
-    
+
     h=get(get(gcf,'children'),'children');
-    
+
     axes_ind      =  findobj(h,'type','Axes');
     legend_ind    =  findobj(h,'type','Legend');
     colorbar_ind  =  findobj(h,'type','Colorbar');
@@ -86,34 +86,34 @@ end
 
 
 for n=1:numel(axes_ind)
-    
+
     % edit x-ticks color
     for m=1:numel(axes_ind(n).XTickLabel)
         axes_ind(n) .XTickLabel{m} = ['\color[rgb]' sprintf('{%f,%f,%f}%s',textcolor)    axes_ind(n) .XTickLabel{m} ];
     end
-    
+
     % edit y-ticks color
     for m=1:numel( axes_ind(n).YTickLabel)
         axes_ind(n).YTickLabel{m} = ['\color[rgb]' sprintf('{%f,%f,%f}%s',textcolor)    axes_ind(n).YTickLabel{m} ];
     end
-    
+
     axes_ind(n) .Color        = tcd{3};% 'none';    % make white area transparent
     axes_ind(n).XColor       = textcolor; % edit x axis color
     axes_ind(n).YColor       = textcolor; % edit y axis color
     axes_ind(n).ZColor       = textcolor; % edit z axis color
-    
+
     axes_ind(n).XLabel.Color = textcolor; % edit x label color
     axes_ind(n).YLabel.Color = textcolor; % edit y label color
     axes_ind(n).ZLabel.Color = textcolor; % edit z label color
-    
+
     axes_ind(n).Title.Color  = textcolor; % edit title text color
-    
+
     % grids have alpha property that needs more care
     axes_ind(n).GridColor =  adjust_color( axes_ind(n).GridColor,tcd,axes_ind(n).GridAlpha);
     axes_ind(n).MinorGridColor =  adjust_color(axes_ind(n).MinorGridColor,tcd,axes_ind(n).GridAlpha);
     axes_ind(n).GridAlpha = 1;
     % axes_ind(n).Subtitle.Color = textcolor;
-    
+
     % take care of other axes children:
     h2 = get(axes_ind(n),'Children');
     g2 = get(axes_ind(n).Children,'type');
@@ -126,7 +126,7 @@ for n=1:numel(axes_ind)
     hist_ind = find(strcmp(g2,'histogram'));
     % contour_ind  = find(strcmp(g2,'contour'));
     % surface_ind = find(strcmp(g2,'surface'));
-    
+
     % edit texts color
     for m=1:numel(text_ind)
         h2(text_ind(m)).Color=adjust_color( h2(text_ind(m)).Color ,tcd);
@@ -134,30 +134,30 @@ for n=1:numel(axes_ind)
             h2(text_ind(m)).BackgroundColor = tcd{3};    %if text has some background color switch to dark bkg theme
         end
     end
-    
+
     % brighten patch colors if dim (use for the case of arrows etc)
     % this might not work well for all patch types so consider to comment
     for m=1:numel(patch_ind)
         h2(patch_ind(m)).FaceColor = adjust_color(h2(patch_ind(m)).FaceColor,tcd);
         h2(patch_ind(m)).EdgeColor = adjust_color(h2(patch_ind(m)).EdgeColor,tcd);
     end
-    
+
     for m=1:numel(line_ind)
         h2(line_ind(m)).Color = adjust_color(h2(line_ind(m)).Color,tcd);
     end
-    
-    
+
+
     for m=1:numel(errorbar_ind)
         h2(errorbar_ind(m)).Color = adjust_color(h2(errorbar_ind(m)).Color,tcd);
         h2(errorbar_ind(m)).MarkerEdgeColor =   adjust_color(h2(errorbar_ind(m)).MarkerEdgeColor,tcd);
         h2(errorbar_ind(m)).MarkerFaceColor = adjust_color(h2(errorbar_ind(m)).MarkerFaceColor,tcd);
     end
-    
+
     for m=1:numel(area_ind)
         h2(area_ind(m)).FaceColor = adjust_color(h2(area_ind(m)).FaceColor,tcd);
         h2(area_ind(m)).EdgeColor = adjust_color(h2(area_ind(m)).EdgeColor,tcd);
     end
-    
+
     for m=1:numel(bar_ind)
         h2(bar_ind(m)).FaceColor = adjust_color(h2(bar_ind(m)).FaceColor,tcd);
         h2(bar_ind(m)).EdgeColor = adjust_color(h2(bar_ind(m)).EdgeColor,tcd);
@@ -168,13 +168,13 @@ for n=1:numel(axes_ind)
         h2(hist_ind(m)).FaceColor = adjust_color(h2(hist_ind(m)).FaceColor,tcd);
         h2(hist_ind(m)).EdgeColor = adjust_color(h2(hist_ind(m)).EdgeColor,tcd);
     end
-    
+
     %       for m=1:numel(contour_ind)
     %         h2(contour_ind(m)).FaceColor = adjust_color(h2(contour_ind(m)).FaceColor,tcd);
     %         h2(contour_ind(m)).EdgeColor = adjust_color(h2(contour_ind(m)).EdgeColor,tcd);
     %     end
-    
-    
+
+
 end
 %% modify Colorbars:
 for n=1:numel(colorbar_ind)
@@ -193,7 +193,7 @@ end
 %% modify polar axes
 for n=1:numel(polaraxes_ind)
 
-    
+
     polaraxes_ind(n) .Color        = tcd{3};% 'none';    % make white area transparent
     polaraxes_ind(n).ThetaColor = textcolor;
     polaraxes_ind(n).RColor = textcolor;
@@ -206,18 +206,18 @@ for n=1:numel(polaraxes_ind)
     polaraxes_ind(n).MinorGridColor =  adjust_color(polaraxes_ind(n).MinorGridColor,tcd,polaraxes_ind(n).GridAlpha);
     polaraxes_ind(n).GridAlpha = 1;
     polaraxes_ind(n).MinorGridAlpha=0.75;
-       
-        for nc=1:size( polaraxes_ind(n).ColorOrder,1)
-             polaraxes_ind(n).ColorOrder(nc,:)=adjust_color(polaraxes_ind(n).ColorOrder(nc,:),tcd);
-        end
 
-         % take care of other axes children:
+    for nc=1:size( polaraxes_ind(n).ColorOrder,1)
+        polaraxes_ind(n).ColorOrder(nc,:)=adjust_color(polaraxes_ind(n).ColorOrder(nc,:),tcd);
+    end
+
+    % take care of other axes children:
     h2 = get(polaraxes_ind(n),'Children');
     g2 = get(polaraxes_ind(n).Children,'type');
     text_ind  = find(strcmp(g2,'text'));
     patch_ind = find(strcmp(g2,'patch'));
     line_ind  = find(strcmp(g2,'line'));
- 
+
     % edit texts color
     for m=1:numel(text_ind)
         h2(text_ind(m)).Color=adjust_color( h2(text_ind(m)).Color ,tcd);
@@ -225,20 +225,20 @@ for n=1:numel(polaraxes_ind)
             h2(text_ind(m)).BackgroundColor = tcd{3};    %if text has some background color switch to dark bkg theme
         end
     end
-    
+
     % brighten patch colors if dim (use for the case of arrows etc)
     % this might not work well for all patch types so consider to comment
     for m=1:numel(patch_ind)
         h2(patch_ind(m)).FaceColor = adjust_color(h2(patch_ind(m)).FaceColor,tcd);
         h2(patch_ind(m)).EdgeColor = adjust_color(h2(patch_ind(m)).EdgeColor,tcd);
     end
-    
+
     for m=1:numel(line_ind)
         h2(line_ind(m)).Color = adjust_color(h2(line_ind(m)).Color,tcd);
-   h2(line_ind(m)).MarkerFaceColor = adjust_color(h2(line_ind(m)).MarkerFaceColor,tcd);
-   h2(line_ind(m)).MarkerEdgeColor = adjust_color(h2(line_ind(m)).MarkerEdgeColor,tcd);
+        h2(line_ind(m)).MarkerFaceColor = adjust_color(h2(line_ind(m)).MarkerFaceColor,tcd);
+        h2(line_ind(m)).MarkerEdgeColor = adjust_color(h2(line_ind(m)).MarkerEdgeColor,tcd);
     end
- end
+end
 
 
 %% modify annotations:
@@ -254,8 +254,8 @@ if ~isempty(ha)
             disp('annotation not available')
             return
         end
-        
-        
+
+
         % edit lineType and shapeType colors
         textboxshape_ind        = find(strcmp(hAnnotChildrenType,'textboxshape'));
         ellipseshape_ind        = find(strcmp(hAnnotChildrenType,'ellipseshape'));
@@ -265,49 +265,49 @@ if ~isempty(ha)
         arrowshape_ind          = find(strcmp(hAnnotChildrenType,'arrowshape'));
         arrow_ind               = find(strcmp(hAnnotChildrenType,'Arrow')); % older Matlab ver
         lineshape_ind           = find(strcmp(hAnnotChildrenType,'lineshape'));
-        
-        
+
+
         for m=1:numel(textboxshape_ind)
             hAnnotChildren(textboxshape_ind(m)).Color      =  textcolor;
             hAnnotChildren(textboxshape_ind(m)).EdgeColor  =  adjust_color(hAnnotChildren(textboxshape_ind(m)).EdgeColor);
         end
-        
+
         for m=1:numel(ellipseshape_ind)
             hAnnotChildren(ellipseshape_ind(m)).Color      =  adjust_color(hAnnotChildren(ellipseshape_ind(m)).Color,tcd);
             hAnnotChildren(ellipseshape_ind(m)).FaceColor  =  adjust_color(hAnnotChildren(ellipseshape_ind(m)).FaceColor,tcd);
         end
-        
+
         for m=1:numel(rectangleshape_ind)
             hAnnotChildren(rectangleshape_ind(m)).Color      =  adjust_color(hAnnotChildren(rectangleshape_ind(m)).Color,tcd);
             hAnnotChildren(rectangleshape_ind(m)).FaceColor  =  adjust_color(hAnnotChildren(rectangleshape_ind(m)).FaceColor,tcd);
         end
-        
+
         for m=1:numel(textarrowshape_ind)
             hAnnotChildren(textarrowshape_ind(m)).Color      =  adjust_color(hAnnotChildren(textarrowshape_ind(m)).Color,tcd);
             hAnnotChildren(textarrowshape_ind(m)).TextColor  =  textcolor;
             hAnnotChildren(textarrowshape_ind(m)).TextEdgeColor = adjust_color(hAnnotChildren(textarrowshape_ind(m)).TextEdgeColor,tcd);
         end
-        
+
         for m=1:numel(doubleendarrowshape_ind)
             hAnnotChildren(doubleendarrowshape_ind(m)).Color = adjust_color(hAnnotChildren(doubleendarrowshape_ind(m)).Color,tcd);
         end
-        
+
         for m=1:numel(arrowshape_ind)
             hAnnotChildren(arrowshape_ind(m)).Color = adjust_color(hAnnotChildren(arrowshape_ind(m)).Color,tcd);
         end
-        
+
         for m=1:numel(arrow_ind)
             hAnnotChildren(arrow_ind(m)).Color = adjust_color(hAnnotChildren(arrow_ind(m)).Color,tcd);
         end
-        
+
         for m=1:numel(lineshape_ind)
             hAnnotChildren(lineshape_ind(m)).Color = adjust_color(hAnnotChildren(lineshape_ind(m)).Color,tcd);
         end
-        
-        
+
+
     end
-    
-    
+
+
 end
 
 function  out=adjust_color(varargin)
@@ -321,19 +321,19 @@ function  out=adjust_color(varargin)
 % via desaturation and brightness to be more legible.
 % the function uses fminbnd, if you dont have the toolbox to use it you can
 % replace it with fmintx (avaiable in Matlab's file exchange)
- 
+
 if nargin==1
-   in = varargin{1};
-   tcd= {[1 1 1],4.5,[0.16 0.16 0.16]};
-   alpha=1;
-   elseif nargin==2
-   in = varargin{1};
-   tcd= varargin{2};
-   alpha=1;
-   elseif nargin==3
-   in = varargin{1};
-   tcd= varargin{2};
-   alpha= varargin{3};
+    in = varargin{1};
+    tcd= {[1 1 1],4.5,[0.16 0.16 0.16]};
+    alpha=1;
+elseif nargin==2
+    in = varargin{1};
+    tcd= varargin{2};
+    alpha=1;
+elseif nargin==3
+    in = varargin{1};
+    tcd= varargin{2};
+    alpha= varargin{3};
 
 end
 
@@ -382,55 +382,55 @@ br=@(in,x) hsv2rgb( bsxfun(@power,rgb2hsv(in),[ones(numel(x),1) ones(numel(x),1)
 
 
 if cr(in)<tcd{2} % default is 4.5
-    
+
     %check if color is just black and replace with perceptual "middle gray"
     if ~sum(in)
         fun0 = @(x) abs(Lstar( (ones(1,3)*x-dark_bkg_assumption ))-0.5);
         L_factor=fminbnd(fun0,0.3,1);
-        
+
         out = ones(1,3)*L_factor;
         return
-        
+
     end
-    
-    
+
+
     % if saturation is what reduce contrast then desaturate
     in_hsv=rgb2hsv(in);
     if in_hsv(2)>0.5
-        
+
         fun1=@(x) abs(cr(ds(in,x))-tcd{2});
         [ds_factor, val]=fminbnd(fun1,0,in_hsv(2));
-        
+
         if val<1e-2
             out = ds(in,ds_factor);
             return
         end
     end
-    
+
     % desaturation alone didn't solve it, try to increase brightness
     fun2 =  @(x) abs(cr(br(in,x))-tcd{2});
     [br_factor, val]=fminbnd(fun2,0,1);
-    
+
     if val<1e-2 && Lstar(br(in,br_factor))>0.5
         out = br(in,br_factor);
         return
     end
-    
+
     % if niether worked then brightening + desaturation:
     fun3 = @(x) abs(cr(ds(br(in,br_factor),x))-tcd{2});
     [brds_factor, val]=fminbnd(fun3,0,1);
-    
+
     if val<1e-2 && Lstar(ds(br(in,br_factor),brds_factor))>0.5
         out = ds(br(in,br_factor),brds_factor);
         return
-        
+
     end
-    
+
     % if all fails treat the color as black as above:
     fun0 = @(x) abs(Lstar( (ones(1,3)*x-dark_bkg_assumption ))-0.5);
     L_factor=fminbnd(fun0,0.3,1);
     out = ones(1,3)*L_factor;
-    
+
 else
     out = in ;
 end
